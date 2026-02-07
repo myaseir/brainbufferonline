616fdbb19d718ddb7834b753412e8ebb4b190290
Muhammad Yasir <yasirabbasi0351@gmail.com>
2026-02-07 16:01:48 +0500

    Added

--- frontend/src/app/components/Auth.jsx
+++ frontend/src/app/components/Auth.jsx
@@ -294,21 +294,21 @@ export default function Auth({ onLoginSuccess }) {
 
             <div className="mt-10 flex flex-col sm:flex-row items-center justify-center lg:justify-start gap-6">
   <a 
-    href="/BrainBuffer.apk" 
+    href="https://github.com/myaseir/brainbufferofficial/releases/download/v4.7.0/BrainBuffer.apk" 
     download="BrainBuffer.apk"
     /* Changed w-full to w-fit and added md:px-12 for a more substantial look on laptop */
     className="w-fit min-w-[240px] text-center bg-slate-900 text-white px-8 md:px-12 py-5 rounded-2xl font-black uppercase tracking-widest text-[10px] md:text-[11px] hover:bg-green-600 transition-all shadow-2xl active:scale-95 flex items-center justify-center gap-3"
   >
     <Smartphone size={16} className="text-green-400" />
-    Download BrainBuffer v4.0
+    Download BrainBuffer v4.7
   </a>
   
   <div className="flex flex-col items-center lg:items-start">
     <p className="text-[9px] md:text-[10px] text-slate-400 font-black uppercase tracking-[0.2em]">
-      Current Build: <span className="text-slate-900">v4.0.0-Stable</span>
+      Current Build: <span className="text-slate-900">v4.7.0-Stable</span>
     </p>
     <p className="text-[9px] md:text-[10px] text-slate-300 font-bold uppercase tracking-widest">
-      Size: 14MB • Android 6.0+
+      Size: 25MB • Android 6.0+
     </p>
   </div>
 </div>
